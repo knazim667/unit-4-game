7da1555dcceedd4aa8fdca2bf290be83cb84671e
Nazam <nazamzahoor@yahoo.com>
2018-06-29 01:53:05 -0700

    98% work is done

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -13,6 +13,8 @@ var stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
 var stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+
+//testing all random number
 console.log("computerGuess" + computerGuess);
 console.log("stone1 " + stone1);
 console.log("stone2 " + stone2);
@@ -34,4 +36,58 @@ function reset () {
     stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
     stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
 
+
 }
+
+//create a click function for images generate number
+$(document).ready(function() {
+    
+
+$("#stone1").on("click", function() {
+    
+            score = score + stone1;
+            $("#score").html(score);
+            console.log("score1 " + score);
+});
+    
+$("#stone2").on("click", function() {
+    
+            score = score + stone2;
+            $("#score").html(score);
+            console.log("score2 " + score);
+});
+    
+$("#stone3").on("click", function() {
+    
+            score = score + stone3;
+            $("#score").html(score);
+            console.log("score3 " + score);
+});
+
+$("#stone4").on("click", function() {
+    
+            score = score + stone4;
+            $("#score").html(score);
+            console.log("score4 " + score);
+});
+
+    // create win and lost
+
+if(score === computerGuess) {
+    
+    wins++;
+    console.log(wins);
+    $("#winn").html("You Win");
+    $("#win").html(wins);
+    reset();
+} else if (score > computerGuess) {
+    
+    lost++;
+    console.log(lost);
+    $("#winn").html("You Lost");
+    $("#lost").html(lost);
+    reset();
+}
+    
+});
+
