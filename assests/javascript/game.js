805b5ba63573365186a0b4724add56f2550660ac
Nazam <nazamzahoor@yahoo.com>
2018-06-28 23:49:37 -0700

    half work done its really hard to design and making random numbers for

---
+++ assests/javascript/game.js
@@ -0,0 +1,37 @@
+//creating variables
+
+var wins = 0;
+var lost = 0;
+var score = 0;
+
+// Create a computer generator number for match
+var computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+$("#randomNumber").html(computerGuess);
+
+//converting the string in integer
+var stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
+var stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+console.log("computerGuess" + computerGuess);
+console.log("stone1 " + stone1);
+console.log("stone2 " + stone2);
+console.log("stone3 " + stone3);
+console.log("stone4 " + stone4);
+
+//creating reset function the game
+
+function reset () {
+    
+    score = 0;
+    $("#score").html(score);
+    computerGuess = parseInt(Math.floor(Math.random() * 120) + 19);
+    $("#randomNumber").html(computerGuess);
+
+
+    stone1 = parseInt(Math.floor(Math.random() * 12) + 1);
+    stone2 = parseInt(Math.floor(Math.random() * 12) + 1);
+    stone3 = parseInt(Math.floor(Math.random() * 12) + 1);
+    stone4 = parseInt(Math.floor(Math.random() * 12) + 1);
+
+}
